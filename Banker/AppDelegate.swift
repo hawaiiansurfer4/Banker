d9e90057961150ce1608bd76022b467c637738e9
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-04 17:02:26 -0400

    Adds LocalState to persist hasOnboarded

--- Banker/AppDelegate.swift
+++ Banker/AppDelegate.swift
@@ -11,24 +11,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
+    var hasOnboarded = false
+
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyVC()
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        
+
         loginViewController.delegate = self
         onboardingContainerViewController.delegateOn = self
         dummyViewController.logoutDelegate = self
-        
+
         window?.rootViewController = loginViewController
 //        window?.rootViewController = onboardingContainerViewController
 //        window?.rootViewController = OnboardingViewController(imageName: "world", titleText: "peace to the world")
-        
+
         return true
     }
 
@@ -36,12 +37,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        setRootViewCOntroller(onboardingContainerViewController)
+        if hasOnboarded {
+            setRootViewCOntroller(dummyViewController)
+        } else {
+            setRootViewCOntroller(onboardingContainerViewController)
+        }
     }
 }
 
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
+        hasOnboarded = true
         setRootViewCOntroller(dummyViewController)
     }
 }
@@ -53,14 +59,14 @@ extension AppDelegate {
             self.window?.makeKeyAndVisible()
             return
         }
-        
+
         window.rootViewController = vc
         window.makeKeyAndVisible()
         UIView.transition(with: window,
-                          duration: 0.3,
-                          options: .transitionCrossDissolve,
-                          animations: nil,
-                          completion: nil)
+            duration: 0.3,
+            options: .transitionCrossDissolve,
+            animations: nil,
+            completion: nil)
     }
 }
 
---
+++ Banker/Utils/LogalState.swift
@@ -0,0 +1,25 @@
+//
+//  LogalState.swift
+//  Banker
+//
+//  Created by Sean Murphy on 6/4/22.
+//
+
+import Foundation
+
+public class LocalState {
+    
+    private enum Keys: String {
+        case hasOnboarded
+    }
+    
+    public static var hasOnboarded: Bool {
+        get {
+            return UserDefaults.standard.bool(forKey: Keys.hasOnboarded.rawValue)
+        }
+        set(newValue) {
+            UserDefaults.standard.set(newValue, forKey: Keys.hasOnboarded.rawValue)
+            UserDefaults.standard.synchronize()
+        }
+    }
+}
