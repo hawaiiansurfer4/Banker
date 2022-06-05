24f00ddbc206755664714b35fc220e397460c2cd
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-05 17:26:57 -0400

    feat: Adds viewModel

--- Banker/AccountSummary/Cells/AccountSummaryCell.swift
+++ Banker/AccountSummary/Cells/AccountSummaryCell.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class AccountSummaryCell: UITableViewCell {
     
+    enum AccountType: String {
+        case Banking
+        case CreditCard
+        case Investment
+    }
+    
+    struct ViewModel {
+        let accountType: AccountType
+        let accountName: String
+//        let balance: Decimal
+        
+//        var balanceAsAttributedString: NSAttributedString {
+//            return CurrencyFormatter().makeAttributedCurrency(balance)
+//        }
+    }
+    
+    let viewModel: ViewModel? = nil
+    
     let typeLabel = UILabel()
     let underlineView = UIView()
     let nameLabel = UILabel()
@@ -122,5 +140,25 @@ extension AccountSummaryCell {
         rootString.append(centString)
         
         return rootString
+    }
+}
+
+extension AccountSummaryCell {
+    func configure(with vm: ViewModel) {
+        
+        typeLabel.text = vm.accountType.rawValue
+        nameLabel.text = vm.accountName
+        
+        switch vm.accountType {
+        case .Banking:
+            underlineView.backgroundColor = appColor
+            balanceLabel.text = "Current Balance"
+        case .CreditCard:
+            underlineView.backgroundColor = .systemOrange
+            balanceLabel.text = "Current Balance"
+        case .Investment:
+            underlineView.backgroundColor = .systemPurple
+            balanceLabel.text = "Value"
+        }
     }
 }
