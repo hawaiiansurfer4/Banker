4b3753abd0fe3176529ec78d3135ae9457ead8da
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-06 16:10:26 -0400

    feat: Shake the bell

--- Banker/Login/LoginViewController.swift
+++ Banker/Login/LoginViewController.swift
@@ -155,12 +155,12 @@ extension LoginViewController {
             return
         }
         
-        if username.isEmpty || password.isEmpty {
-            configureView(withMessage: "Username / password cannot be blank")
-            return
-        }
+//        if username.isEmpty || password.isEmpty {
+//            configureView(withMessage: "Username / password cannot be blank")
+//            return
+//        }
         
-        if username == "K" && password == "A" {
+        if username == "" && password == "" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
             return
