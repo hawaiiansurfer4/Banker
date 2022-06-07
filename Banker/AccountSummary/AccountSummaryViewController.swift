792f2cd498a36d462aa3356c1a41c007f9e33b7e
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-07 14:24:35 -0400

    feat: Handles Network Errors

--- Banker/AccountSummary/AccountSummaryViewController.swift
+++ Banker/AccountSummary/AccountSummaryViewController.swift
@@ -134,7 +134,7 @@ extension AccountSummaryViewController {
             case .success(let profile):
                 self.profile = profile
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -145,7 +145,7 @@ extension AccountSummaryViewController {
             case .success(let accounts):
                 self.accounts = accounts
             case .failure(let error):
-                print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -175,6 +175,27 @@ extension AccountSummaryViewController {
                 accountName: $0.name,
                 balance: $0.amount)
         }
+    }
+    
+    private func displayError(_ error: NetworkError) {
+        let title: String
+        let message: String
+        switch error {
+        case .decodingError:
+            title = "Decoding Error"
+            message = "We could not process your request. Please try again."
+        case .serverError:
+            title = "Server Error"
+            message = "Ensure you are connected to the internet. Please try again."
+        }
+        showErrorAlert(title: title, message: message)
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
