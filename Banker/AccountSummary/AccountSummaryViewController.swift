602e9057d2b834d7647df6c3cdbceefe0b7be75d
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-06 20:09:40 -0400

    feat: group account summary network calls together

--- Banker/AccountSummary/AccountSummaryViewController.swift
+++ Banker/AccountSummary/AccountSummaryViewController.swift
@@ -27,21 +27,18 @@ class AccountSummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        setupNavigationBar()
     }
-    
-    func setupNavigationBar() {
-        navigationItem.rightBarButtonItem = logoutBarButtonItem
-    }
+
 }
 
 extension AccountSummaryViewController {
     private func setup() {
+        setupNavigationBar()
         setupTableView()
         setupTableHeaderView()
-//        fetchAccounts()
-        fetchDataAndLoadView()
+        fetchData()
     }
+    
     
     private func setupTableView() {
         tableView.backgroundColor = appColor
@@ -71,6 +68,11 @@ extension AccountSummaryViewController {
         headerView.frame.size = size
         
         tableView.tableHeaderView = headerView
+    }
+    
+    
+    func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = logoutBarButtonItem
     }
 }
 
@@ -105,27 +107,35 @@ extension AccountSummaryViewController {
 
 //MARK: - Networking
 extension AccountSummaryViewController {
-    private func fetchDataAndLoadView() {
+    private func fetchData() {
+        let group = DispatchGroup()
         
+        group.enter()
         fetchProfile(forUserId: "1") { results in
             switch results {
             case .success(let profile):
                 self.profile = profile
                 self.configureTableHeaderView(with: profile)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
         }
+        
+        group.enter()
         fetchAccounts(forUserId: "1") { result in
             switch result {
             case .success(let accounts):
                 self.accounts = accounts
                 self.configureTableCells(with: accounts)
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            self.tableView.reloadData()
         }
     }
     
